148c88512719c871ed487fb654af1dade06595ae
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2023-09-02 11:51:22 +0530

    session test

--- src/passport-config.js
+++ src/passport-config.js
@@ -34,7 +34,9 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
-    console.log("\n deserializeUser-user:",user);
+    if (!user) {
+      return done(null, false);
+    }
     done(null, user);
   } catch (error) {
     console.log("\n deserializeUser-err:",error);

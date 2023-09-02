93b0e795ca40882ba5ae8aff90ad4bc16971c681
GOPIKRISHNA-M-A <gopikrishna6003@gmail.com>
2023-09-02 12:17:05 +0530

    session test

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    secret: 'secret',
     resave: false,
     saveUninitialized: false,
     store: store,
